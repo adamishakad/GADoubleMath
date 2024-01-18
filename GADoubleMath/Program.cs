31899547f6bea3a01eb3d73fbdaae95bf31f265b
Adam Ishak <iadam9862@gmail.com>
2024-01-17 18:41:20 -0800

    Updeated Program File

--- GADoubleMath/Program.cs
+++ GADoubleMath/Program.cs
@@ -20,7 +20,36 @@
             // Console.WriteLine(result4)
             // double result5 = 9.1 + 4.3;
             // Console.WriteLine(result5)
-            // 
+            // double result6 = 15.7 - 8.2;
+            // Console.WriteLine(result6)
+            // double result7 = 3.0 * 5.5;
+            // Console.WriteLine(result7)	
+            // double result8 = 24.8 / 4.0;
+            // Console.WriteLine(result8)	
+            // double result9 = 7.2 + 2.3;
+            // Console.WriteLine(result9)	
+            // double result10 = 18.5 - 6.4;
+            // Console.WriteLine(result10)	
+            // double result11 = 4.5 * 6.0;
+            // Console.WriteLine(result11)	
+            // double result12 = 30.2 / 5.0;
+            // Console.WriteLine(result12)	
+            // double result13 = 9.8 + 7.2;
+            // Console.WriteLine(result13)	
+            //double result14 = 22.4 - 11.2;
+            // Console.WriteLine(result14)	
+            // double result15 = 5.5 * 3.0;
+            // Console.WriteLine(result15)
+            // double a = 0.1;
+            // double b = 0.2;
+            // double sum = a + b;
+            Console.WriteLine($"Equation 1 Result: {equation1}");
+            Console.WriteLine($"Equation 2 Result: {equation2}");
+
+
+
+
+
         }
     }
 }
