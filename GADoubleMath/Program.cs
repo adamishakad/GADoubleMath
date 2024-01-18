64958f9d4591d5cc4792643014ec5f05eaa12b83
Adam Ishak <iadam9862@gmail.com>
2024-01-17 18:00:17 -0800

    Updated Prograg File

--- GADoubleMath/Program.cs
+++ GADoubleMath/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // double myDouble = 123.456;
+            // double sum = 5.6 + 3.2;
+            // double difference = 10.5 - 4.3;
+            // double product = 2.5 * 3.0;
+            // double quotient = 12.6 / 2.0;
+            // double result1 = 5.6 + 3.2; 
+            // Console.WriteLine((result1);
+            // double result2 = 10.5 - 4.3;
+            // Console.WriteLine(result2)
+            // double result3 = 2.5 * 3.0;
+            // Console.WriteLine(result3)
+            // double result4 = 12.6 / 2.0;
+            // Console.WriteLine(result4)
+            // double result5 = 9.1 + 4.3;
         }
     }
 }
